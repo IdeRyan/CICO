bb7f6a6587c355c86a7f90b2b3e471ed32f8cf6c
Idealy Ryan <idealryan34@gmail.com>
2026-01-28 19:11:44 +0300

    ArticleDAO finished

--- src/main/java/mg/cico/DAO/ArticleDAO.java
+++ src/main/java/mg/cico/DAO/ArticleDAO.java
@@ -23,22 +23,26 @@ public class ArticleDAO {
     private final Connection c = DBConnection.startConnection();
     private final Logger logger = LoggerFactory.getLogger(ArticleDAO.class);
 
+    // Ajouter un article
     public void addArticle(Article a){
-        String sql = "INSERT INTO article VALUES(?,?,?,?,?,?,?)";
-        try(PreparedStatement stmt = c.prepareStatement(sql) ) {
+        String sql = """
+                INSERT INTO article(reference, designation, unite, etat, dateAchat)
+                VALUES(?,?,?,?,?)
+                """;
+        try(PreparedStatement stmt = c.prepareStatement(sql)) {
             stmt.setString(1, a.getReference());
-            stmt.setString(2 , a.getDesignation());
-            stmt.setString(3 , a.getUnite());
-            stmt.setDouble(4 , a.getQuantite());
-            stmt.setObject(5, a.getEtat());
-            stmt.setObject(6, Date.valueOf(a.getDateAchat()));
-            stmt.setInt(7, a.getIdProjet());
-            logger.debug("Article {} added succesfully",a.getDesignation());
+            stmt.setString(2, a.getDesignation());
+            stmt.setString(3, a.getUnite());
+            stmt.setObject(4, a.getEtat().name());
+            stmt.setDate(5, Date.valueOf(a.getDateAchat()));
+            stmt.executeUpdate();
+            logger.debug("Article {} ajouté avec succès", a.getDesignation());
         } catch (SQLException e) {
-            logger.error("Error while adding article {}", a.getDesignation() , e.getMessage());
+            logger.error("Erreur lors de l'ajout de l'article {}", a.getDesignation(), e);
         }
     }
 
+    // Récupérer tous les articles
     public List<Article> getAllArticle(){
         String sql = "SELECT * FROM article";
         List<Article> articles = new ArrayList<>();
@@ -50,149 +54,98 @@ public class ArticleDAO {
                 a.setReference(rs.getString("reference"));
                 a.setDesignation(rs.getString("designation"));
                 a.setUnite(rs.getString("unite"));
-                a.setQuantite(rs.getDouble("quantite"));
-                a.setEtat(rs.getObject("etat"));
-                a.setDateAchat((rs.getObject("dateAchat",java.time.LocalDate.class)));
-                a.setIdProjet(rs.getInt("idProjet"));
+                a.setEtat(Etat.valueOf(rs.getString("etat")));
+                a.setDateAchat(rs.getObject("dateAchat", LocalDate.class));
                 articles.add(a);
             }
 
-            logger.debug("Getting all the article succesffully");
+            logger.debug("Récupération de tous les articles réussie");
             return articles;
         } catch (SQLException e) {
-            logger.error("Error while getting all the article :",e.getMessage());
+            logger.error("Erreur lors de la récupération des articles :", e);
             return Collections.emptyList();
         }
     }
 
-    public void editDesignation(String d , Article a){
-        String sql = "UPDATE article SET designation=? WHERE reference=?";
+    // Mettre à jour un article
+    public void updateArticle(Article a){
+        String sql = """
+            UPDATE article 
+            SET designation=?, unite=?, etat=?, dateAchat=?
+            WHERE reference=?
+            """;
         try(PreparedStatement stmt = c.prepareStatement(sql)) {
-            stmt.setString(1, d);
-            stmt.setString(2, a.getReference());
-            stmt.execute();
-            logger.debug("Designation of {} modified to {}",a.getReference(),d);
+            stmt.setString(1, a.getDesignation());
+            stmt.setString(2, a.getUnite());
+            stmt.setObject(3, a.getEtat().name());
+            stmt.setDate(4, Date.valueOf(a.getDateAchat()));
+            stmt.setString(5, a.getReference());
+            stmt.executeUpdate();
+            logger.debug("Article {} mis à jour avec succès", a.getReference());
         } catch (SQLException e) {
-            logger.error("Error while editing the designation of {} :",a.getReference(),e.getMessage());
+            logger.error("Erreur lors de la mise à jour de l'article {}", a.getReference(), e);
         }
     }
 
-    public void editQuantite(Double q , Article a){
-        String sql = "UPDATE article SET quantite=? WHERE reference=?";
-        try(PreparedStatement stmt = c.prepareStatement(sql)) {
-            stmt.setDouble(1, q);
-            stmt.setString(2, a.getReference());
-            stmt.execute();
-            logger.debug("Qunatity of {} modified to {}",a.getReference(),q);
-        } catch (SQLException e) {
-            logger.error("Error while editing the quantity of {} :",a.getReference(),e.getMessage());
-        }
-    }
-
-    public void editEtat(Etat et , Article a){
-        String sql = "UPDATE article SET etat=? WHERE reference=?";
-        try(PreparedStatement stmt = c.prepareStatement(sql)) {
-            stmt.setObject(1, et);
-            stmt.setString(2, a.getReference());
-            stmt.execute();
-            logger.debug("State of {} modified to {}",a.getReference(),et);
-        } catch (SQLException e) {
-            logger.error("Error while editing the state of {} :",a.getReference(),e.getMessage());
-        }
-    }
-
-
-    private void addQuantiteFilter(StringBuilder sql, List<Object> params, Double min, Double max) {
-
-    if (min != null && max != null) {
-        sql.append(" AND quantite BETWEEN ? AND ?");
-        params.add(min);
-        params.add(max);
-    } else if (min != null) {
-        sql.append(" AND quantite >= ?");
-        params.add(min);
-    } else if (max != null) {
-        sql.append(" AND quantite <= ?");
-        params.add(max);
-    }
-}
-
-    private void addDateFilter(StringBuilder sql, List<Object> params, String column, LocalDate from, LocalDate to) {
-        if (from != null && to != null) {
-            sql.append(" AND ").append(column).append(" BETWEEN ? AND ?");
-            params.add(from);
-            params.add(to);
-        } else if (from != null) {
-            sql.append(" AND ").append(column).append(" = ?");
-            params.add(from);
-        }
-    }
+    // Recherche par critères (sans idProjet ni quantité)
     public List<Article> searchByCriteria(ArticleSearchCriteria criteria) {
 
-    List<Article> resultat = new ArrayList<>();
-    StringBuilder sql = new StringBuilder("SELECT * FROM article WHERE 1=1");
-    List<Object> params = new ArrayList<>();
+        List<Article> resultat = new ArrayList<>();
+        StringBuilder sql = new StringBuilder("SELECT * FROM article WHERE 1=1");
+        List<Object> params = new ArrayList<>();
 
-    if (criteria.getKeyword() != null && !criteria.getKeyword().isBlank()) {
-        sql.append(" AND (reference ILIKE ? OR designation ILIKE ? OR unite ILIKE ?)");
-        for (int i = 0; i < 3; i++) {
-            params.add("%" + criteria.getKeyword() + "%");
-        }
-    }
-
-    if (criteria.getEtat() != null) {
-        sql.append(" AND etat = ?");
-        params.add(criteria.getEtat().name());
-    }
-    
-    if (criteria.getIdProjet() != null) {
-        sql.append(" AND idProjet = ?");
-        params.add(criteria.getIdProjet());
-    }
-
-    addQuantiteFilter(
-        sql,
-        params,
-        criteria.getQuantiteMin(),
-        criteria.getQuantiteMax()
-    );
-
-    addDateFilter(
-        sql,
-        params,
-        "dateAchat",
-        criteria.getDateAchatFrom(),
-        criteria.getDateAchatTo()
-    );
-
-    sql.append(" ORDER BY designation ASC");
-
-    try (PreparedStatement stmt = c.prepareStatement(sql.toString())) {
-
-        for (int i = 0; i < params.size(); i++) {
-            stmt.setObject(i + 1, params.get(i));
+        // Filtre par mot-clé
+        if (criteria.getKeyword() != null && !criteria.getKeyword().isBlank()) {
+            sql.append(" AND (reference ILIKE ? OR designation ILIKE ? OR unite ILIKE ?)");
+            for (int i = 0; i < 3; i++) {
+                params.add("%" + criteria.getKeyword() + "%");
+            }
         }
 
-        ResultSet rs = stmt.executeQuery();
-
-        while (rs.next()) {
-            Article a = new Article();
-            a.setReference(rs.getString("reference"));
-            a.setDesignation(rs.getString("designation"));
-            a.setUnite(rs.getString("unite"));
-            a.setQuantite(rs.getDouble("quantite"));
-            a.setEtat(Etat.valueOf(rs.getString("etat")));
-            a.setDateAchat(rs.getObject("dateAchat", LocalDate.class));
-            a.setIdProjet(rs.getInt("idProjet"));
-            resultat.add(a);
+        // Filtre par état
+        if (criteria.getEtat() != null) {
+            sql.append(" AND etat = ?");
+            params.add(criteria.getEtat().name());
         }
 
-        logger.debug("Recherche Article : {} résultat(s) trouvé(s)", resultat.size());
-        return resultat;
+        // Filtre par dateAchat
+        if (criteria.getDateAchatFrom() != null && criteria.getDateAchatTo() != null) {
+            sql.append(" AND dateAchat BETWEEN ? AND ?");
+            params.add(criteria.getDateAchatFrom());
+            params.add(criteria.getDateAchatTo());
+        } else if (criteria.getDateAchatFrom() != null) {
+            sql.append(" AND dateAchat >= ?");
+            params.add(criteria.getDateAchatFrom());
+        } else if (criteria.getDateAchatTo() != null) {
+            sql.append(" AND dateAchat <= ?");
+            params.add(criteria.getDateAchatTo());
+        }
 
-    } catch (SQLException e) {
-        logger.error("Erreur lors de la recherche des articles", e);
-        return Collections.emptyList();
+        sql.append(" ORDER BY designation ASC");
+
+        try (PreparedStatement stmt = c.prepareStatement(sql.toString())) {
+            for (int i = 0; i < params.size(); i++) {
+                stmt.setObject(i + 1, params.get(i));
+            }
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Article a = new Article();
+                a.setReference(rs.getString("reference"));
+                a.setDesignation(rs.getString("designation"));
+                a.setUnite(rs.getString("unite"));
+                a.setEtat(Etat.valueOf(rs.getString("etat")));
+                a.setDateAchat(rs.getObject("dateAchat", LocalDate.class));
+                resultat.add(a);
+            }
+
+            logger.debug("Recherche Article : {} résultat(s) trouvé(s)", resultat.size());
+            return resultat;
+
+        } catch (SQLException e) {
+            logger.error("Erreur lors de la recherche des articles", e);
+            return Collections.emptyList();
+        }
     }
-}
 }
--- src/main/java/mg/cico/models/criteria/ArticleSearchCriteria.java
+++ src/main/java/mg/cico/models/criteria/ArticleSearchCriteria.java
@@ -5,18 +5,11 @@ import java.time.LocalDate;
 import mg.cico.models.enums.Etat;
 
 public class ArticleSearchCriteria {
-    
+
     private String keyword;
-
     private Etat etat;
-
     private LocalDate dateAchatFrom;
     private LocalDate dateAchatTo;
-
-    private Double quantiteMin;
-    private Double quantiteMax;
-
-    private Integer idProjet;
 
     public String getKeyword() {
         return keyword;
@@ -49,30 +42,4 @@ public class ArticleSearchCriteria {
     public void setDateAchatTo(LocalDate dateAchatTo) {
         this.dateAchatTo = dateAchatTo;
     }
-
-    public Double getQuantiteMin() {
-        return quantiteMin;
-    }
-
-    public void setQuantiteMin(Double quantiteMin) {
-        this.quantiteMin = quantiteMin;
-    }
-
-    public Double getQuantiteMax() {
-        return quantiteMax;
-    }
-
-    public void setQuantiteMax(Double quantiteMax) {
-        this.quantiteMax = quantiteMax;
-    }
-
-    public Integer getIdProjet() {
-        return idProjet;
-    }
-
-    public void setIdProjet(Integer idProjet) {
-        this.idProjet = idProjet;
-    }
-
-    
 }
